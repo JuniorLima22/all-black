6400f0695d78cbe5088463fd40f1f58364868a67
Junior Lima <juniorlima.dev@gmail.com>
2022-09-02 12:11:07 -0300

    feat: adicionando validação dos dados do formulário

--- public/cadastrar.php
+++ public/cadastrar.php
@@ -2,22 +2,46 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use App\Entity\Cliente;
+use App\Helpers\Validate;
+
+$validate = new Validate();
 
 if (isset($_POST['nome'], $_POST['documento'])) {
-    $obCliente = new Cliente;
-    $obCliente->nome = $_POST['nome'];
-    $obCliente->documento = $_POST['documento'];
-    $obCliente->cep = $_POST['cep'];
-    $obCliente->endereco = $_POST['endereco'];
-    $obCliente->bairro = $_POST['bairro'];
-    $obCliente->cidade = $_POST['cidade'];
-    $obCliente->uf = $_POST['uf'];
-    $obCliente->telefone = $_POST['telefone'];
-    $obCliente->email = $_POST['email'];
-    $obCliente->ativo = $_POST['ativo'];
+
+    $validate->validate([
+        'nome' => ['max:200', 'min:3', 'required'],
+        'documento' => ['numeric', 'required'],
+        'cep' => ['numeric', 'required'],
+        'endereco' => ['max:255', 'min:3', 'required'],
+        'bairro' => ['max:255', 'required'],
+        'cidade' => ['max:100', 'required'],
+        'uf' => ['max:2', 'required'],
+        'telefone' => ['numeric', 'max:20'],
+        'ativo' => ['exists:SIM,NÂO', 'required'],
+    ]);
+    
+    if (!empty($_POST['email'])) {
+        $validate->validate([
+            'email' => ['max:255', 'email'],
+        ]);
+    }
+
+    if (count($validate->errors()) == 0) {
+        $obCliente = new Cliente;
+        $obCliente->nome = $_POST['nome'];
+        $obCliente->documento = $_POST['documento'];
+        $obCliente->cep = $_POST['cep'];
+        $obCliente->endereco = $_POST['endereco'];
+        $obCliente->bairro = $_POST['bairro'];
+        $obCliente->cidade = $_POST['cidade'];
+        $obCliente->uf = $_POST['uf'];
+        $obCliente->telefone = $_POST['telefone'];
+        $obCliente->email = $_POST['email'];
+        $obCliente->ativo = $_POST['ativo'];
 
     if ($obCliente->cadastrar()) {
-        header('Location: index.php?status=success'); exit;
+            header('Location: index.php?status=success');exit;
+        }
     }
 }
 
